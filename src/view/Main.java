82c3a3cda627dd60dc2be643130ac7ddd72d9881
JMB <jbruel@gmail.com>
2021-05-24 10:18:24 +0200

    Update src files

---
+++ src/view/Main.java
@@ -0,0 +1,24 @@
+package view;
+
+import java.awt.EventQueue;
+
+import view.app.LoginDialog;
+
+/**
+ * Main qui permet d'exécuter l'application
+ */
+public class Main {
+    public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginDialog frame = new LoginDialog();
+			        frame.setLocationRelativeTo(null);
+			        frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
